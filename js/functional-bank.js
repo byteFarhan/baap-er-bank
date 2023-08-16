c1cd1864d9d1cd7371b784abdedd31869ba7319c
farhan-71 <farhanmazumder75@gmail.com>
2023-08-16 14:29:40 +0600

    functional intarection is added.

---
+++ js/functional-bank.js
@@ -0,0 +1,69 @@
+// all id names
+/* 
+btn-withdraw
+btn-deposit
+deposit-total
+withdraw-total
+balance-total
+withdraw-fild
+deposit-fild
+ */
+
+// utilitis functions
+function getElementTextById(elementId) {
+  const element = document.getElementById(elementId);
+  const elementText = element.innerText;
+  const elementTextNum = parseFloat(elementText);
+  return elementTextNum;
+}
+
+function getInputValueById(inputId) {
+  const input = document.getElementById(inputId);
+  const inputValue = input.value;
+  input.value = "";
+  if (inputValue === "") {
+    alert("Pleace put your amount first!");
+    return 0;
+  } else if (isNaN(inputValue)) {
+    alert("Invalid input!");
+    return 0;
+  } else if (inputValue == 0 || inputValue < 0) {
+    alert("Don't try to fun!");
+    return 0;
+  }
+  const inputValueNum = parseFloat(inputValue);
+  return inputValueNum;
+}
+function setNewAmount(newTotal, elementToSet) {
+  const element = document.getElementById(elementToSet);
+  element.innerText = newTotal;
+}
+// deposit part
+document.getElementById("btn-deposit").addEventListener(`click`, function () {
+  const depositTotal = getElementTextById("deposit-total");
+  //   console.log(depositTotal);
+  const depositInputValue = getInputValueById("deposit-fild");
+  //   console.log(depositInputValue);
+  const newDepositTotal = depositTotal + depositInputValue;
+  setNewAmount(newDepositTotal, "deposit-total");
+
+  const totalBalance = getElementTextById("balance-total");
+  //   console.log(totalBalanceElement);
+  //   const totalBalanceElement = document.getElementById("balance-total");
+  const newBalance = totalBalance + depositInputValue;
+  setNewAmount(newBalance, "balance-total");
+});
+
+// withdraw part
+document.getElementById("btn-withdraw").addEventListener(`click`, function () {
+  const withdrawTotal = getElementTextById("withdraw-total");
+  //   console.log(withdrawTotal);
+  const inputAmount = getInputValueById("withdraw-fild");
+  //   console.log(inputAmount);
+  const newWithdrawTotal = withdrawTotal + inputAmount;
+  setNewAmount(newWithdrawTotal, "withdraw-total");
+
+  const totalBalance = getElementTextById("balance-total");
+  const newTotalBalance = totalBalance - inputAmount;
+  setNewAmount(newTotalBalance, "balance-total");
+});
--- js/login.js
+++ js/login.js
@@ -2,6 +2,9 @@ document.getElementById("btn-submit").addEventListener("click", function () {
   const email = document.getElementById("user-email").value;
   const password = document.getElementById("user-password").value;
   // console.log(email, password);
+  document.getElementById("user-email").value = "";
+  document.getElementById("user-password").value = "";
+
   if (email === "baap@sontan.com" && password === "secret") {
     window.location.href = "bank.html";
     // console.log("valid input");
